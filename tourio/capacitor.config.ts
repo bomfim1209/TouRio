0192c3ef7d44567a3652a023175647fc83f95497
Matheus Bomfim <matheus.bomfim@uol.com.br>
2024-09-08 22:44:29 -0300

    primeiro commit

---
+++ tourio/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tourio',
+  webDir: 'www'
+};
+
+export default config;
