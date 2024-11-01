13cf43ef012f279d8473f828ab3f6c2dbf367318
Laboratorio 303C <bslab303c@uniacad.edu.br>
2024-11-01 10:21:16 -0300

    firebase

--- tourio/src/app/login/login.page.ts
+++ tourio/src/app/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
+import { ToastController, LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,12 +9,48 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  email: string = '';
+  password: string = '';
+  isAuthenticated: boolean = false;
 
-  constructor(private rota: Router) {}
+  constructor(private rota: Router,
+    private afAuth: AngularFireAuth,
+    private toastController: ToastController,
+    private loadingController: LoadingController
+  ) {}
   
-  redirecionar(){
+  async login() {
+    const loading = await this.loadingController.create({
+    message: 'Carregando...',
+    });
+    await loading.present();
+    try {
+    const user = await this.afAuth.signInWithEmailAndPassword(this.email, this.password);
+    await loading.dismiss();
+    this.isAuthenticated = true;
+    this.showToast('ESTAMOS CONECTADOS');
     this.rota.navigateByUrl('/apresentacao');
+    } catch (error) {
+    await loading.dismiss();
+    this.showToast('NÃO ESTAMOS CONECTADOS');
+    }
   }
+
+  async logout() {
+      await this.afAuth.signOut();
+      this.isAuthenticated = false;
+      this.showToast('DESCONECTADOS COM SUCESSO');
+    }
+    
+    async showToast(message: string) {
+      const toast = await this.toastController.create({
+      message,
+      duration: 2000
+      });
+      toast.present();
+    }
+    
+    
 
   ngOnInit() {
   }
